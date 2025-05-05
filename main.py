f17df1ef5ebf717fe961f9a8c741cb59971cfa6d
saram ali <140950904+SARAMALI15792@users.noreply.github.com>
2025-05-05 23:47:27 +0500

    updated the file and add the requirements

--- main.py
+++ main.py
@@ -16,8 +16,45 @@ llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash',
 async def main():
     # Create agent with the model
     agent = Agent(
-        task="go on youtube and search for campusx and play the lanchain video and give me the view about the channel and person name also !!",
-        llm=llm
+        task="""
+        
+        Task Overview
+Please execute the following steps in order:
+
+Create Google Account:
+
+Navigate to Google Account creation.
+
+Register a new personal Google account with the following details:
+
+First Name: Sahil
+
+Last Name: Kumar
+
+Email: sahilkumar15792@gmail.com
+
+Password: sahil123
+
+Send Email Notification:
+
+After account creation, log into the newly created Gmail account.
+
+Compose and send an email to: saramali15792@gmail.com
+
+Subject: Account Created
+
+Body: Write something about browser-use agent.
+
+ChatGPT Signup and Interaction:
+
+Go to ChatGPT and sign up using the created Gmail account.
+
+After successful signup and login, ask a Python-related question in ChatGPT
+             
+             
+              """,      
+         
+         llm=llm
     )
     await agent.run()
 
